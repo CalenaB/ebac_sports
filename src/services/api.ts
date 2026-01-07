1e984f2e46543c3cfa9400d873e17c9d5842667c
Calena Bittencourt <calena.bittencourt@gmail.com>
2026-01-07 20:34:29 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Produto } from '../types/Produto'
 
 export const api = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
     baseUrl: 'https://api-ebac.vercel.app/api/ebac_sports'
-    }),
-    endpoints: (builder) => ({
+  }),
+  endpoints: (builder) => ({
     getProdutos: builder.query<Produto[], void>({
-        query: () => ''
+      query: () => '/sports'
     })
-    })
+  })
 })
 
 export const { useGetProdutosQuery } = api
+
