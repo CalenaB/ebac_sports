25e6f5c90c59a13f65608529354ac911a1611690
Calena Bittencourt <calena.bittencourt@gmail.com>
2026-01-07 20:37:01 -0300

    Update Produtos.tsx

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,43 +1,51 @@
-import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
+import { Produto as ProdutoType } from '../types/Produto'
 
 import * as S from './styles'
 
 type Props = {
-  produtos: ProdutoType[]
   favoritos: ProdutoType[]
   adicionarAoCarrinho: (produto: ProdutoType) => void
   favoritar: (produto: ProdutoType) => void
 }
 
 const ProdutosComponent = ({
-  produtos,
   favoritos,
   adicionarAoCarrinho,
   favoritar
 }: Props) => {
+  const { data: produtos = [], isLoading, error } = useGetProdutosQuery()
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    const idsDosFavoritos = favoritos.map((f) => f.id)
 
-    return IdsDosFavoritos.includes(produtoId)
+    return idsDosFavoritos.includes(produtoId)
+  }
+
+  if (isLoading) {
+    return <p>Carregando produtos...</p>
+  }
+
+  if (error) {
+    return <p>Erro ao carregar os produtos</p>
   }
 
   return (
-    <>
-      <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-            produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
-          />
-        ))}
-      </S.Produtos>
-    </>
+    <S.Produtos>
+      {produtos.map((produto) => (
+        <Produto
+          key={produto.id}
+          produto={produto}
+          estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+          favoritar={favoritar}
+          aoComprar={adicionarAoCarrinho}
+        />
+      ))}
+    </S.Produtos>
   )
 }
 
 export default ProdutosComponent
+
